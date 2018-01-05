193e02b01e2d63aea65f652c8da290e699aa833b
Joshua Getega <jgetega@gmail.com>
2018-01-05 10:23:30 -0500

    Enforce use of python3 to set up virtual environment

--- vagrant/venv_bootstrap.sh
+++ vagrant/venv_bootstrap.sh
@@ -3,6 +3,6 @@
 export HOME=/home/vagrant
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv -a /home/vagrant/AcademicIntegrityToolV2 -r /home/vagrant/AcademicIntegrityToolV2/AcademicIntegrityToolV2/requirements/local.txt AcademicIntegrityToolV2 
+mkvirtualenv --python=python3 -a /home/vagrant/AcademicIntegrityToolV2 -r /home/vagrant/AcademicIntegrityToolV2/AcademicIntegrityToolV2/requirements/local.txt AcademicIntegrityToolV2
 workon AcademicIntegrityToolV2
 python manage.py migrate
