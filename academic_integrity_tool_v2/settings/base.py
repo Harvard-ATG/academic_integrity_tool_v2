4d0e501e699b48eb7918073e6bb0ec19475e1eba
Joshua Getega <jgetega@gmail.com>
2018-01-08 21:23:23 -0500

    Remove debug_toolbar addition redundancy in INSTALLED_APPS

--- academic_integrity_tool_v2/settings/base.py
+++ academic_integrity_tool_v2/settings/base.py
@@ -35,7 +35,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 # Authentication
