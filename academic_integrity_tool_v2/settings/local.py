7d5d5823bf765873b5807671027961ac13eb392d
Joshua Getega <jgetega@gmail.com>
2018-01-08 19:34:53 -0500

    Add debug_toolbar.middleware.DebugToolbarMiddleware to MIDDLEWARE

--- academic_integrity_tool_v2/settings/local.py
+++ academic_integrity_tool_v2/settings/local.py
@@ -8,7 +8,7 @@ SECRET_KEY = '1@7&11tb*l1c84uco-9=%(u#mb)_dl6%%++rihgnl&r)wmldrc'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 INSTALLED_APPS.extend(['debug_toolbar'])
-MIDDLEWARE_CLASSES.extend(['debug_toolbar.middleware.DebugToolbarMiddleware'])
+MIDDLEWARE.extend(['debug_toolbar.middleware.DebugToolbarMiddleware'])
 
 # For Django Debug Toolbar:
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
