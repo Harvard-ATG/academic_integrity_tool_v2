5c24410e0ca19288169ca77dabc2f174a92b9b35
Joshua Getega <jgetega@gmail.com>
2018-01-19 16:43:07 -0500

    Comment out the debug_toolbar import for now - just to enable successful deployment to tlt-ops dev environment

--- academic_integrity_tool_v2/urls.py
+++ academic_integrity_tool_v2/urls.py
@@ -22,9 +22,11 @@ urlpatterns = [
     path('', include('policy_wizard.urls')),
 ]
 
+"""
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
+"""
