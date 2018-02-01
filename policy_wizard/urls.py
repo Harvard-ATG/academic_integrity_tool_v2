252188ee6664bba9772293546aa19f9cd3a3107a
Joshua Getega <jgetega@gmail.com>
2018-02-01 18:11:00 -0500

    Change 'technologist' to 'Administrator'

--- policy_wizard/urls.py
+++ policy_wizard/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('technologist', views.admin_level_template_view, name='admin_level_template_list'),
+    path('administrator', views.admin_level_template_view, name='admin_level_template_list'),
     path('instructor', views.PolicyTemplateListView.as_view(), name='policy_template_list'),
     path('student', views.published_policy_to_display_view, name='published_policy_to_display'),
     path('template/<int:pk>/edit', views.PolicyTemplateUpdateView.as_view(), name='policy_template_edit'),
