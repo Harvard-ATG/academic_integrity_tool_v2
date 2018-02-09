bd8863ac8fd951e129d6c585229e4836147899c1
Joshua Getega <jgetega@gmail.com>
2018-02-09 03:32:43 -0500

    Make context_id field in Policies model non-nullable and required

--- policy_wizard/models.py
+++ policy_wizard/models.py
@@ -9,10 +9,11 @@ class PolicyTemplates(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
 
 class Policies(models.Model):
-    context_id = models.CharField(max_length=255)
+    context_id = models.CharField(max_length=255, blank=False)
     related_template = models.ForeignKey(PolicyTemplates, null=True, on_delete=models.CASCADE, related_name="related_policies")
     is_published = models.BooleanField()
     published_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='related_policies')
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
