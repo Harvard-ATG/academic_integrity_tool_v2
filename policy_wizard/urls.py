1c378cad17becf854481ad372b4e919c261a16b9
Joshua Getega <jgetega@gmail.com>
2018-02-09 01:33:41 -0500

    Modify naming of student role MVC for semantic appeal

--- policy_wizard/urls.py
+++ policy_wizard/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     #path('administrator', views.admin_level_template_view, name='admin_level_template_list'),
     #path('instructor', views.instructor_level_template_view, name='policy_template_list'),
 
-    path('student', views.published_policy_to_display_view, name='published_policy_to_display'),
+    path('student', views.student_published_policy_view, name='student_published_policy'),
     path('template/<int:pk>/edit', views.AdminLevelTemplateUpdateView.as_view(), name='admin_level_template_edit'),
     path('policy/<int:pk>/edit', views.instructor_level_policy_edit_view, name='instructor_level_policy_edit'),
     path('published_policy/<int:pk>/', views.instructor_published_policy, name='instructor_published_policy'),
