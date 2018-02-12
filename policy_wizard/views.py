b70a15fe6d57294a5662d7da2a582728670248ae
Joshua Getega <jgetega@gmail.com>
2018-02-12 10:15:57 -0500

    Use Django sessions to handle roles

--- policy_wizard/views.py
+++ policy_wizard/views.py
@@ -16,50 +16,59 @@ from .middleware import role_identifier, validate_request
 @csrf_exempt
 def process_lti_launch_request_view(request):
 
+    #True if this is a typical lti launch. False if not.
     is_basic_lti_launch = request.method == 'POST' and request.POST.get(
         'lti_message_type') == 'basic-lti-launch-request'
 
+    #True if this request is valid. False if not.
     request_is_valid = validate_request(request)
 
-    if is_basic_lti_launch and request_is_valid:
+    if is_basic_lti_launch and request_is_valid: #if typical lti launch and request is valid ...
 
-        #Store the context_id in the request's session attribute
+        #Store the context_id in the request's session attribute.
+        #A 'context_id' represents a particular course.
         request.session['context_id'] = request.POST.get('context_id')
 
-        #store context_id in dictionary
-        #lti_launch_params_dict['context_id'] = request.POST.get('context_id')
+        #Figure out role of launcher and store the role in the request's session attribute
+        request.session['role'] = role_identifier(request.POST.get('ext_roles'))
 
-        #Figure out role of launcher and path to take
-        role = role_identifier(request.POST.get('ext_roles'))
-
-        if role=='Instructor':
-            return redirect('policy_templates_list', role='Instructor')
+        #Using the role, e.g. 'Administrator', 'Instructor', or 'Student', determine route to take
+        role = request.session['role']
+        if role=='Administrator' or role=='Instructor':
+            return redirect('policy_templates_list')
         elif role=='Student':
             return redirect('student_published_policy')
-        elif role=='Administrator':
-            return redirect('policy_templates_list', role='Administrator')
-    else:
+    else: #if not typical lti launch or if request is not valid ...
         raise PermissionDenied
 
-def policy_templates_list_view(request, role):
+def policy_templates_list_view(request):
+
+    #Fetch role from session attribute. It will be either 'Administrator' or 'Instructor'.
+    role = request.session['role']
 
     if role=='Instructor':
-        try:
+        try: #if a policy has already been published for this course ...
+            #Get the published policy
             publishedPolicy = Policies.objects.get(context_id=request.session['context_id'])
+            #Render the published policy
             return render(request, 'instructor_published_policy.html', {'publishedPolicy': publishedPolicy})
-        except ObjectDoesNotExist:
+        except ObjectDoesNotExist: #If no such policy exists ...
             pass
 
+    #Fetch each policy template from the `PolicyTemplates` table in the database and store as a variable
     written_work_policy_template = PolicyTemplates.objects.get(name="Collaboration Permitted: Written Work")
     problem_sets_policy_template = PolicyTemplates.objects.get(name="Collaboration Permitted: Problem Sets")
     collaboration_prohibited_policy_template = PolicyTemplates.objects.get(name="Collaboration Prohibited")
     custom_policy_template = PolicyTemplates.objects.get(name="Custom Policy")
 
     if role=='Administrator':
+        #Django template to use
         template_to_use = 'admin_level_template_list.html'
     else: #role=='Instructor'
+        # Django template to use
         template_to_use = 'instructor_level_template_list.html'
 
+    #Render the policy templates
     return render(
         request,
         template_to_use,
@@ -72,10 +81,9 @@ def policy_templates_list_view(request, role):
 
 class AdminLevelTemplateUpdateView(UpdateView):
     model = PolicyTemplates
-    #fields = ['name', 'body']
     fields = ['body']
     template_name = 'admin_level_template_edit.html'
-    success_url = reverse_lazy('policy_templates_list', kwargs={'role': 'Administrator'})
+    success_url = reverse_lazy('policy_templates_list')
 
 def instructor_level_policy_edit_view(request, pk):
     policyTemplate = get_object_or_404(PolicyTemplates, pk=pk)
@@ -127,7 +135,7 @@ def instructor_delete_old_publish_new_view(request, pk):
     #Delete old policy
     Policies.objects.filter(pk=pk).delete()
     #Redirect to list of templates
-    return redirect('policy_templates_list', role='Instructor')
+    return redirect('policy_templates_list')
 
 
 
