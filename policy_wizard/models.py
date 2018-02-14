d60b8bef40e10c50984abed89352b5fd0cc35e8e
Joshua Getega <jgetega@gmail.com>
2018-02-14 16:35:10 -0500

    Change 'body' field of 'Policies' model from a TextField to a HTMLField for tinymce integration

--- policy_wizard/models.py
+++ policy_wizard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce import models as tinymce_models
 
 # Create your models here.
 class PolicyTemplates(models.Model):
@@ -13,7 +14,7 @@ class Policies(models.Model):
     related_template = models.ForeignKey(PolicyTemplates, null=True, on_delete=models.CASCADE, related_name="related_policies")
     is_published = models.BooleanField()
     published_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='related_policies')
-    body = models.TextField()
+    body = tinymce_models.HTMLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
