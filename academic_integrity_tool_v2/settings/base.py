62b05971a2c2fa56227c14374a234aa4d031aced
Joshua Getega <jgetega@gmail.com>
2018-02-15 10:18:00 -0500

    Add some sample tinyMCE configuration settings for testing

--- academic_integrity_tool_v2/settings/base.py
+++ academic_integrity_tool_v2/settings/base.py
@@ -212,3 +212,11 @@ PYLTI_CONFIG = {
         }
     }
 }
+
+
+TINYMCE_DEFAULT_CONFIG = {
+    'plugins': "table,spellchecker,paste,searchreplace",
+    'theme': "advanced",
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 10,
+}
