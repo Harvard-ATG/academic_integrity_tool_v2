c5f92cf737b6ad6130e1e3f4e3bb96a96bec108b
Joshua Getega <jgetega@gmail.com>
2018-03-06 12:31:09 -0500

    Remove labels from body fields so they are not displayed on the edit screens

--- policy_wizard/forms.py
+++ policy_wizard/forms.py
@@ -3,12 +3,16 @@ from .models import Policies
 from tinymce.widgets import TinyMCE
 from django.forms import ModelForm, Textarea
 from .models import PolicyTemplates, Policies
+from django.utils.translation import gettext_lazy as _
 
 class NewPolicyForm(ModelForm):
 
     class Meta:
         model = Policies
         fields = ['body']
+        labels = {
+            'body': _(''),
+        }
         widgets = {
             'body': Textarea(attrs={'cols': 80, 'rows': 1000}),
         }
@@ -17,6 +21,9 @@ class PolicyTemplateForm(ModelForm):
     class Meta:
         model = PolicyTemplates
         fields = ['body']
+        labels = {
+            'body': _(''),
+        }
         widgets = {
             'body': Textarea(attrs={'cols': 80, 'rows': 10}),
         }
