9e7115d7f1494e9b120b5b34bfc59b5143b056de
Arthur Barrett <abarrett@fas.harvard.edu>
2018-03-08 12:26:04 -0500

    Updated tests with travis-ci.

--- README.md
+++ README.md
@@ -1,30 +1,63 @@
-**Academic Integrity Policy Tool (V2)**
+# Academic Integrity Policy Tool (V2)
 
 This is a django application that enables instructors to prepare academic integrity policies from templates made by instructional technologists and publish said policies for students to view. It is an LTI tool that is embedded into and launched from the Canvas LMS.
 
-**Steps to get it running locally:**
-* Ensure you have VirtualBox installed on your local machine
+## Steps to get it running locally
+
+### With Vagrant
+
+* Ensure you have [VirtualBox](https://www.virtualbox.org/) and [Vagrant](https://www.vagrantup.com/) installed on your local machine
 * Clone this repository to a desired directory location on your local machine
-* From the CLI and in the project root directory, which should contain a Vagrantfile, run `vagrant up`
+* From the CLI and in the project root directory, which should contain a `Vagrantfile`, run `vagrant up`
 * Log into the local VM with `vagrant ssh`
 * Do `workon academic_integrity_tool_v2`
 * Run `./manage.py runserver 0.0.0.0:8000`
+* Open your browser and navigate to `http://localhost:8000`
 
-**Installing the tool in the Canvas LMS:**
+### With Docker
+
+* Ensure you have [Docker](https://www.docker.com/) installed on your local machine
+* Clone this repository to a desired directory location on your local machine
+* Do `cp academic_integrity_v2/settings/secure.py.example academic_integrity_v2/settings/secure.py` and then update:
+	- `db_default_host: 'db'` 
+	- `redis_host: 'redis'`
+* From the CLI and in the project root directory, which should container a `Dockerfile` and `docker-compose.yml` file, run `docker-compose up`
+* Open your browser and navigate to `http://localhost:8000`
+
+## Installing the tool in the Canvas LMS:**
+
 * Log into your Harvard Canvas account and select a desired course
-* On the left navigation bar, go to “Settings”
-* From top horizontal bar, click “Apps”
-* Click the blue “App” button. An “Add App” form should pop up.
-* On the form, on the “Configuration Type” drop down menu, select “Paste XML”. The form structure will update accordingly.
-* In the ‘Name’ field, type “Academic Integrity Tool”, without the quotation marks.
-* For the 'Consumer Key' and 'Shared Secret' fields, from a new CLI window, access and use the values in academic_integrity_tool_v2/academic_integrity_tool_v2/settings/secure.py
-* On a new browser tab, go to 'localhost:8000/lti/config.xml'
+* On the left navigation bar, go to "Settings"
+* From top horizontal bar, click "Apps"
+* Click the blue "App" button. An "Add App" form should pop up.
+* On the form, on the "Configuration Type" drop down menu, select "Paste XML". The form structure will update accordingly.
+* In the ‘Name’ field, type "Academic Integrity Tool", without the quotation marks.
+* For the 'Consumer Key' and 'Shared Secret' fields, from a new CLI window, access and use the values in `academic_integrity_tool_v2/academic_integrity_tool_v2/settings/secure.py`
+* On a new browser tab, go to 'http://localhost:8000/lti/config.xml'
 * Copy the XML output
-* Go back to your browser tab with the “Add App” form, In the ‘XML Configuration’ field, paste in the XML output you copied in the step above.
+* Go back to your browser tab with the "Add App" form, In the ‘XML Configuration’ field, paste in the XML output you copied in the step above.
 * Change the 5th-to-last line of the XML you just pasted in from
 <lticm:property name="default">disabled</lticm:property>
 to
 <lticm:property name="default">enabled</lticm:property>
-* Click the blue “Submit” button.
-* You should see the tool listed as “Academic Integrity Tool” in the left navigation pane of Canvas.
+* Click the blue "Submit" button.
+* You should see the tool listed as "Academic Integrity Tool" in the left navigation pane of Canvas.
 * Click that "Academic Integrity Tool" navigation item to launch the tool
+
+## Running Tests
+
+With vagrant: 
+
+```
+$ vagrant ssh 
+$ cd /vagrant 
+$ python manage.py test
+```
+
+With docker: 
+
+```
+$ docker-compose up
+$ docker-compose run web python manage.py test
+```
+
--- policy_wizard/tests.py
+++ policy_wizard/tests.py
@@ -1,66 +1,186 @@
 from django.test import TestCase, RequestFactory
-from django.urls import reverse, resolve
+from django.shortcuts import reverse
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.contrib.auth.models import User
-from .views import process_lti_launch_request_view
+from django.core.exceptions import PermissionDenied
 from .models import Policies, PolicyTemplates
-from .views import student_published_policy_view, policy_templates_list_view
-from .middleware import lti_launch_params_dict
+from . import views
+
+import pylti
+import mock
 
 
-# Create your tests here.
+def annotate_request_with_session(request, params=None):
+    middleware = SessionMiddleware()
+    middleware.process_request(request)
+    if params is not None:
+        for k, v in params.items():
+            request.session[k] = v
+    return request
+
+def create_default_policy_templates():
+    policies = [
+        PolicyTemplates.objects.create(name="Collaboration Permitted: Written Work", body="Foo"),
+        PolicyTemplates.objects.create(name="Collaboration Permitted: Problem Sets", body="Bar"),
+        PolicyTemplates.objects.create(name="Collaboration Prohibited", body="Bar"),
+        PolicyTemplates.objects.create(name="Custom Policy", body="Bar"),
+    ]
+    return policies
+
+
+class LtiLaunchTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def testGetRequest(self):
+        request = self.factory.get('process_lti_launch_request')
+        annotate_request_with_session(request)
+        with self.assertRaises(pylti.common.LTIException):
+            views.process_lti_launch_request_view(request)
+
+    def testPostIsEmpty(self):
+        request = self.factory.post('process_lti_launch_request')
+        annotate_request_with_session(request)
+        with self.assertRaises(pylti.common.LTIException):
+            views.process_lti_launch_request_view(request)
+
+    @mock.patch('policy_wizard.views.validate_request')
+    def testPostNotValidLtiRequest(self, mock_validate_request):
+        mock_validate_request.return_value = False
+        request = self.factory.post('process_lti_launch_request', {
+            'lti_message_type': 'basic-lti-launch-request',
+        })
+        annotate_request_with_session(request)
+        with self.assertRaises(PermissionDenied):
+            views.process_lti_launch_request_view(request)
+
+    @mock.patch('policy_wizard.views.validate_request')
+    def testPostValidLtiRequest(self, mock_validate_request):
+        mock_validate_request.return_value = True
+        request = self.factory.post('process_lti_launch_request', {
+            'lti_message_type': 'basic-lti-launch-request',
+            'ext_roles': 'urn:lti:instrole:ims/lis/Student,urn:lti:role:ims/lis/Learner,urn:lti:sysrole:ims/lis/User',
+        })
+        annotate_request_with_session(request)
+        response = views.process_lti_launch_request_view(request)
+        self.assertTrue(response.status_code, 200)
+
+    @mock.patch('policy_wizard.views.validate_request')
+    def testStudentLaunch(self, mock_validate_request):
+        mock_validate_request.return_value = True
+        postparams = {
+            'lti_message_type': 'basic-lti-launch-request',
+            'context_id': 'abcd1234',
+            'ext_roles': 'urn:lti:instrole:ims/lis/Student,urn:lti:role:ims/lis/Learner,urn:lti:sysrole:ims/lis/User',
+        }
+        request = self.factory.post('process_lti_launch_request', postparams)
+        annotate_request_with_session(request)
+        response = views.process_lti_launch_request_view(request)
+        self.assertTrue(response.status_code, 301)
+        self.assertEquals(response['Location'], reverse('student_published_policy'))
+        self.assertEquals(request.session['context_id'], postparams['context_id'])
+        self.assertEquals(request.session['role'], 'Student')
+
+    @mock.patch('policy_wizard.views.validate_request')
+    def testInstructorLaunch(self, mock_validate_request):
+        mock_validate_request.return_value = True
+        postparams = {
+            'lti_message_type': 'basic-lti-launch-request',
+            'context_id': 'abcd1234',
+            'ext_roles': 'urn:lti:role:ims/lis/Instructor,urn:lti:instrole:ims/lis/Student,urn:lti:sysrole:ims/lis/User',
+        }
+        request = self.factory.post('process_lti_launch_request', postparams)
+        annotate_request_with_session(request)
+        response = views.process_lti_launch_request_view(request)
+        self.assertTrue(response.status_code, 301)
+        self.assertEquals(response['Location'], reverse('policy_templates_list'))
+        self.assertEquals(request.session['context_id'], postparams['context_id'])
+        self.assertEquals(request.session['role'], 'Instructor')
+
+    @mock.patch('policy_wizard.views.validate_request')
+    def testAdministratorLaunch(self, mock_validate_request):
+        mock_validate_request.return_value = True
+        postparams = {
+            'lti_message_type': 'basic-lti-launch-request',
+            'context_id': 'abcd1234',
+            'ext_roles': 'urn:lti:instrole:ims/lis/Administrator,urn:lti:instrole:ims/lis/Instructor,urn:lti:instrole:ims/lis/Student,urn:lti:sysrole:ims/lis/User',
+        }
+        request = self.factory.post('process_lti_launch_request', postparams)
+        annotate_request_with_session(request)
+        response = views.process_lti_launch_request_view(request)
+        self.assertTrue(response.status_code, 301)
+        self.assertEquals(response['Location'], reverse('policy_templates_list'))
+        self.assertEquals(request.session['context_id'], postparams['context_id'])
+        self.assertEquals(request.session['role'], 'Administrator')
+
 
 class AdministratorRoleTests(TestCase):
 
     def setUp(self):
-        self.policyTemplate1 = PolicyTemplates.objects.create(name="Collaboration Permitted: Written Work", body="Foo")
-        self.policyTemplate2 = PolicyTemplates.objects.create(name="Collaboration Permitted: Problem Sets", body="Bar")
-        self.policyTemplate3 = PolicyTemplates.objects.create(name="Collaboration Prohibited", body="Bar")
-        self.policyTemplate4 = PolicyTemplates.objects.create(name="Custom Policy", body="Bar")
+        self.factory = RequestFactory()
+        self.policyTemplates = create_default_policy_templates()
+
+    def tearDown(self):
+        for policyTemplate in self.policyTemplates:
+            policyTemplate.delete()
 
     def test_administrator_launch(self):
-        url = reverse('policy_templates_list', kwargs={'role': 'Administrator'})
-        response = self.client.get(url)
+        request = self.factory.get('policy_templates_list')
+        annotate_request_with_session(request, {
+            'context_id': 'fgvsxrzpdbcmiuawhwet',
+            'role': 'Administrator'
+        })
+        response = views.policy_templates_list_view(request)
         self.assertEquals(response.status_code, 200)
 
-class studentRoleTests(TestCase):
+class InstructorRoleTests(TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
+        self.policyTemplates = create_default_policy_templates()
+
+    def tearDown(self):
+        for policyTemplate in self.policyTemplates:
+            policyTemplate.delete()
+
+    def test_instructor_launch(self):
+        request = self.factory.get('policy_templates_list')
+        annotate_request_with_session(request, {
+            'context_id': 'tlhzlqzolkhapmnoukgm',
+            'role': 'Instructor'
+        })
+        response = views.policy_templates_list_view(request)
+        self.assertEquals(response.status_code, 200)
+
+
+class StudentRoleTests(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.policyTemplates = create_default_policy_templates()
+
         self.user = User.objects.create_user(
             username='jacob', email='jacob@…', password='top_secret')
 
+        self.context_id = 'z2gd5dn5nkg7tb5et6fb'
+
         self.publishedPolicy = Policies.objects.create(
-            context_id='z2gd5dn5nkg7tb5et6fb',
+            context_id=self.context_id,
             published_by=self.user,
             is_published=True,
             body='important policy'
         )
 
-    def test_student_lti_launch_status_code(self):
-        url = reverse('student_published_policy')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+    def tearDown(self):
+        for policyTemplate in self.policyTemplates:
+            policyTemplate.delete()
 
     def test_student_published_policy_view(self):
-        lti_launch_params_dict['context_id'] = self.publishedPolicy.context_id
         request = self.factory.get('student_published_policy')
-        response = student_published_policy_view(request)
+        annotate_request_with_session(request, {
+            'context_id': self.context_id,
+            'role': 'Student'
+        })
+        response = views.student_published_policy_view(request)
         self.assertEquals(response.status_code, 200)
         self.assertTrue(Policies.objects.exists())
-
-
-class InstructorRoleTests(TestCase):
-
-    def setUp(self):
-        self.policyTemplate1 = PolicyTemplates.objects.create(name="Collaboration Permitted: Written Work", body="Foo")
-        self.policyTemplate2 = PolicyTemplates.objects.create(name="Collaboration Permitted: Problem Sets", body="Bar")
-        self.policyTemplate3 = PolicyTemplates.objects.create(name="Collaboration Prohibited", body="Bar")
-        self.policyTemplate4 = PolicyTemplates.objects.create(name="Custom Policy", body="Bar")
-
-    def test_instructor_launch(self):
-        url = reverse('policy_templates_list', kwargs={'role': 'Instructor'})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-
-
-
