033e48a33776ef5d9448d8d328272e25d8cb72ff
Joshua Getega <jgetega@fas.harvard.edu>
2018-07-02 16:14:34 -0400

    Refactor permission-checking-decorators

--- policy_wizard/decorators.py
+++ policy_wizard/decorators.py
@@ -1,28 +1,34 @@
+from functools import wraps
 from django.core.exceptions import PermissionDenied
 
-def require_role_administrator(some_function):
-
-    def wrapper(request, pk):
-        if request.session['role'] == 'Administrator':
-            return some_function(request, pk)
+def require_role_administrator(view_function):
+    @wraps(view_function)
+    def wrapper(request, *args, **kwargs):
+        role = request.session.get('role')
+        if role == 'Administrator':
+            return view_function(request, *args, **kwargs)
         else:
             raise PermissionDenied
     
     return wrapper
 
-def require_role_instructor(some_function):
-    def wrapper(request, pk):
-        if request.session['role'] == 'Instructor':
-            return some_function(request, pk)
+def require_role_instructor(view_function):
+    @wraps(view_function)
+    def wrapper(request, *args, **kwargs):
+        role = request.session.get('role')
+        if role == 'Instructor':
+            return view_function(request, *args, **kwargs)
         else:
             raise PermissionDenied
 
     return wrapper
 
-def require_role_student(some_function):
-    def wrapper(request):
-        if request.session['role'] == 'Student':
-            return some_function(request)
+def require_role_student(view_function):
+    @wraps(view_function)
+    def wrapper(request, *args, **kwargs):
+        role = request.session.get('role')
+        if role == 'Student':
+            return view_function(request, *args, **kwargs)
         else:
             raise PermissionDenied
 
