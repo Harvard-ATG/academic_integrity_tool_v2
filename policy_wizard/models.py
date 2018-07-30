1794e21a2a8f1e4a639e308867668c0e8e592605
Joshua Getega <jgetega@fas.harvard.edu>
2018-07-30 19:38:25 -0400

    First pass at changes to accomodate data from the previous version of the policy wizard

--- policy_wizard/models.py
+++ policy_wizard/models.py
@@ -12,11 +12,12 @@ class PolicyTemplates(models.Model):
 
 #Published policies
 class Policies(models.Model):
-    context_id = models.CharField(max_length=255, blank=False)
+    course_id = models.IntegerField(null=True)
+    context_id = models.CharField(max_length=255, null=True)
     related_template = models.ForeignKey(PolicyTemplates, null=True, on_delete=models.CASCADE, related_name="related_policies")
-    is_published = models.BooleanField()
+    is_published = models.SmallIntegerField()
     published_by = models.CharField(max_length=255)
-    is_active = models.BooleanField()
+    is_active = models.SmallIntegerField()
     body = tinymce_models.HTMLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
