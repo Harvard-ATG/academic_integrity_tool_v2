97e192c51dc6ac296cc3270a9f8ea616f1584995
Joshua Getega <jgetega@fas.harvard.edu>
2018-08-08 23:17:02 -0400

    Improve role determination by adding logic to process institutional roles. Also ensure role determination happens for all variations of the value of the 'ext_roles' LTI launch parameter.

--- policy_wizard/utils.py
+++ policy_wizard/utils.py
@@ -6,44 +6,48 @@ from lti_provider.lti import LTI
 def role_identifier(ext_roles_text):
     """
     :param ext_roles_text: This will be the value of the 'ext_roles' attribute in the POST request lti forms.
-    It is a string, like 'urn:lti:role:ims/lis/Student,urn:lti:sysrole:ims/lis/User'
+    It is a string, like 'urn:lti:instrole:ims/lis/Administrator,urn:lti:instrole:ims/lis/Instructor,
+    urn:lti:instrole:ims/lis/Student,urn:lti:role:ims/lis/Instructor,urn:lti:sysrole:ims/lis/User'
     :return: A one word string, namely, the actual role. E.g. 'Student'
     """
 
-    # E.g., transform 'urn:lti:role:ims/lis/Student,urn:lti:sysrole:ims/lis/User'
-    # to [urn:lti:role:ims/lis/Student, urn:lti:sysrole:ims/lis/User]
+    # E.g., transform 'urn:lti:instrole:ims/lis/Administrator,urn:lti:instrole:ims/lis/Instructor,
+    # urn:lti:instrole:ims/lis/Student,urn:lti:role:ims/lis/Instructor,urn:lti:sysrole:ims/lis/User'
+    # to [urn:lti:instrole:ims/lis/Administrator, urn:lti:instrole:ims/lis/Instructor,
+    # urn:lti:instrole:ims/lis/Student, urn:lti:role:ims/lis/Instructor, urn:lti:sysrole:ims/lis/User]
     ext_roles_text_as_list = ext_roles_text.split(",")
 
-    # Store the context roles in context_roles and the non-context roles (institution and system roles) in non_context_roles
+    # Store the context roles in context_roles and the institutional roles in institutional_roles. Ignore system roles.
+    # In this app, the institutional roles are used only when there are no context roles.
     # For context/institution/system role reference, see https://www.imsglobal.org/specs/ltiv1p1/implementation-guide#toc-29
     context_roles = []
-    non_context_roles = []
+    institutional_roles = []
     for role in ext_roles_text_as_list:
         if 'lti:role' in role:
             context_roles.append(role)
+        elif 'lti:instrole' in role:
+            institutional_roles.append(role)
         else:
-            non_context_roles.append(role)
-
-    #Determine the 'policy_role', i.e. the role to use in this app, by mapping from context/non_context roles
-    # to the 3 role types (Administrator, Instructor, and Student) in this policy wizard
-    if not bool(context_roles):#if context_roles list is empty, i.e. if there are no context roles...
-        # Process the institutional and system roles
-        for non_context_role in non_context_roles:
-            if ('Administrator') in non_context_role:
-                policy_role = 'Administrator'
-                break
-            else:
-                raise PermissionDenied
-    else: #if context_roles list is not empty
-        context_roles_as_string = ''.join(context_roles) #convert context_roles to string
+            pass
+    # Determine the 'policy_role', i.e. the role to use in this app, by mapping from context/institutional roles in Canvas
+    # to the 3 role types (Administrator, Instructor, and Student) in this policy wizard app
+    if bool(context_roles): #if context_roles list is not empty... (It is expected that if the launcher is registered in the course,
+    # a context role will be present to reflect the launcher's role in the course.)
+        context_roles_as_string = ''.join(context_roles)  # convert context_roles to string
         if ('Administrator' in context_roles_as_string):
             policy_role = 'Administrator'
         elif ('Instructor' in context_roles_as_string) or ('TeachingAssistant' in context_roles_as_string):
             policy_role = 'Instructor'
-        elif ('Learner' in context_roles_as_string) or ('NonCreditLearner' in context_roles_as_string) or ('Mentor' in context_roles_as_string) or ('ContentDeveloper' in context_roles_as_string):
-            policy_role = 'Student'
         else:
-            raise PermissionDenied
+            policy_role = 'Student'
+    else: #if context_roles list is empty... (This can happen in at least 2 cases: 1, when the canvas course site from which the wizard
+    # is launched is current but the launcher is not registered in the course site, or 2, when the canvas course site from which the
+    # wizard is launched is no longer current.)
+        institutional_roles_as_string = ''.join(institutional_roles)  # convert institutional_roles to string
+        if ('Administrator' in institutional_roles_as_string):
+            policy_role = 'Administrator'
+        else:
+            policy_role = 'Student'
 
     return policy_role
 
