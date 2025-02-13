a06ae99c8510f3a321dfc10b207ca26323d5448a
Arthur Barrett <abarrett@fas.harvard.edu>
2025-02-13 10:47:19 -0500

    feat: updated readme and removed vagrant

--- vagrant/venv_bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Set up virtualenv and migrate project
-export HOME=/home/vagrant
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv --python=python3 -a /home/vagrant/academic_integrity_tool_v2 -r /home/vagrant/academic_integrity_tool_v2/academic_integrity_tool_v2/requirements/local.txt academic_integrity_tool_v2
-workon academic_integrity_tool_v2
-python manage.py migrate
